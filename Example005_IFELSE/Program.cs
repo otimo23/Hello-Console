201d6eea101eacece9c1f9dbe7b27a7fb79f92ed
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-10 11:20:58 +0100

    ifelse

---
+++ Example005_IFELSE/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name:");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, this is Masha!");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
