97a1c71f5c23c855a8856f57340cfc0900c2ea8e
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-10 12:44:30 +0100

    half way

---
+++ Examples008_Triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+
+int xa = 3, ya = 1,
+    xb = 3, yb = 20,
+    xc = 20, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+
+int x = xa, y = xb;
+
+int count = 10;
+
+while(count<10)
+{
+    int what = new Random().Next(0, 3); // (0;3) 0 1 2
+    
+}
