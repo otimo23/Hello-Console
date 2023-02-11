fccf6d0dff1eec51946693ef10b3763bc4dbdc2e
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-11 16:30:55 +0100

    if(номер вводит пользователь)

---
+++ Seminar001_if/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter number A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number B: ");
+int B = int.Parse(Console.ReadLine());
+if (A > B)
+{
+    Console.WriteLine("A = max"); 
+    Console.WriteLine("B = min"); 
+} 
+
+if (B > A)
+{
+    Console.WriteLine("A = min"); 
+    Console.WriteLine("B = max");
+}
+if (A ==B)
+{
+    Console.WriteLine("A = B"); 
+
+}
