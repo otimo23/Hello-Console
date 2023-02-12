c9dfb4f2e12261a22476ad7dc7077c296eae453e
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-12 06:56:27 +0100

    max of 3 numbers

---
+++ Homework001_maxof3numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number c: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
