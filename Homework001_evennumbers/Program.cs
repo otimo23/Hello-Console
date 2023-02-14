157fd8e750e1c314ba2eeaaba4e95b733aa32947
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-14 17:31:34 +0100

    на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

---
+++ Homework001_evennumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("enter number from 1 to 100: ");
+int num = int.Parse(Console.ReadLine()!);
+while (num > 1)
+{
+    if (num % 2 ==0)
+    {
+        Console.WriteLine($"even numbers - {num}");
+    }
+    else
+    {
+        num = num - 1;
+        Console.WriteLine($"even numbers - {num}");
+
+    }
+num = num - 1;
+}
